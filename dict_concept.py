2d056dd8593cde87099b84ef0f7735bb3a32098f
Thomas Luna <thomasluna991@gmail.com>
2026-03-26 14:33:43 -0500

    ejercico 1

--- dict_concept.py
+++ dict_concept.py
@@ -1,5 +1,7 @@
-get_config={"width":480,
-"height":480,
-"color_mode":"dark",
-"sensitivity":0.4}
-print(get_config)
+def get_config():
+    return {
+        "width": 480,
+        "height": 480,
+        "color_mode": "dark",
+        "sensitivity": 0.4
+    }
